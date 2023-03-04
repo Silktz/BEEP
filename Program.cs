3d8de0b218f8a0bff68c18aa960a18bf386c3050
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-03 21:47:28 -0300

    So aceita

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("---Pastrozinho---");
+
+Console.WriteLine("Dó"); Console.Beep(1320,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+Console.WriteLine("mí"); Console.Beep(1650,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+
+Thread.Sleep(1500);
+
+Console.WriteLine("Dó"); Console.Beep(1320,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+Console.WriteLine("dó"); Console.Beep(1320,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+
+Thread.Sleep(1500);
+
+Console.WriteLine("Dó"); Console.Beep(1320,200);
+Console.WriteLine("sol"); Console.Beep(1980,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+Console.WriteLine("mí"); Console.Beep(1650,200);
+Console.WriteLine("mí"); Console.Beep(1650,200);
+Console.WriteLine("mí"); Console.Beep(1650,200);
+
+Thread.Sleep(1500);
+
+Console.WriteLine("Dó"); Console.Beep(1320,200);
+Console.WriteLine("ré"); Console.Beep(1485,200);
+Console.WriteLine("mí"); Console.Beep(1650,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+Console.WriteLine("fá"); Console.Beep(1759,200);
+
+Console.WriteLine("THE END....");
+
